16418446e7907d9e5840d49538cec7dfd3dea83e
sergey.chirva <sergey.chirva@adraba.io>
2019-06-22 16:12:37 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar'
+import Users from './components/users/Users'
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+      users: [],
+      loading: false,
+  };
+
+  async componentDidMount() {
+      this.setState( { loading: true })
+
+      const { data } = await axios.get(
+          'https://api.github.com/users',
+          {
+              params: {
+                  client_id: process.env.REACT_APP_GITHUB_CLIENT_ID,
+                  client_secret: process.env.REACT_APP_GITHUB_CLIENT_SECRET,
+              }
+          });
+      this.setState( { loading: false, users: data })
+  }
+
+  render() {
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hello from React@@!'))  // ===
+    return (
+        <div className="App">
+          <Navbar icon='fab fa-github'/>
+          <div className="container">
+            <Users loading={this.state.loading} users={this.state.users} />
+          </div>
+        </div>
+    );
+  }
 }
 
 export default App;

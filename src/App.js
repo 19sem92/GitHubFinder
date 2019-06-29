dfb66aa2270326820c41ff81149934a0323d24f0
sergey.chirva <sergey.chirva@adraba.io>
2019-06-29 15:35:22 +0300

    feat(routing): add routing and page for user and about page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React, { Component } from 'react';
+import React, { Fragment, Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from './components/layout/Navbar'
 import Users from './components/users/Users'
+import User from './components/users/User'
 import Search from './components/users/Search'
 import Alert from './components/layout/Alert'
+import About from './components/pages/About'
 import axios from 'axios';
 import './App.css';
 
 class App extends Component {
   state = {
       users: [],
+      user: {},
       loading: false,
       alert: null,
   };
@@ -42,6 +46,21 @@ class App extends Component {
         this.setState( { loading: false, users: items })
     }
 
+    // Get single Github user
+    getUser = async (username) => {
+        this.setState( { loading: true })
+
+        const { data }  = await axios.get(
+            `https://api.github.com/users/${username}`,
+            {
+                params: {
+                    client_id: process.env.REACT_APP_GITHUB_CLIENT_ID,
+                    client_secret: process.env.REACT_APP_GITHUB_CLIENT_SECRET,
+                }
+            });
+        this.setState( { loading: false, user: data })
+    }
+
     clearUsers = () => {
         this.setState( { users: [], loading: false })
     }
@@ -52,25 +71,46 @@ class App extends Component {
     }
 
   render() {
-    const { loading, users } = this.state
+    const { loading, alert, user, users } = this.state
 
     return (
-        <div className="App">
-          <Navbar icon='fab fa-github'/>
-          <div className="container">
-            <Alert alert={this.state.alert} />
-            <Search
-                searchUsers={this.searchUsers}
-                clearUsers={this.clearUsers}
-                showClear={!!users.length}
-                setAlert={this.setAlert}
-            />
-            <Users
-                loading={loading}
-                users={users}
-            />
-          </div>
-        </div>
+        <Router>
+            <div className="App">
+              <Navbar icon='fab fa-github'/>
+              <div className="container">
+                <Alert alert={alert} />
+                  <Switch>
+                      <Route
+                        exact
+                        path='/'
+                        render={() => (
+                            <Fragment>
+                                <Search
+                                    searchUsers={this.searchUsers}
+                                    clearUsers={this.clearUsers}
+                                    showClear={!!users.length}
+                                    setAlert={this.setAlert}
+                                />
+                                <Users
+                                    loading={loading}
+                                    users={users}
+                                />
+                            </Fragment>
+                        )}
+                      />
+                      <Route exact path='/about' component={About} />
+                      <Route exact path='/user/:login' render={(props) => (
+                          <User
+                              { ...props }
+                              getUser={this.getUser}
+                              user={user}
+                              loading={loading}
+                          />
+                      )} />
+                  </Switch>
+              </div>
+            </div>
+        </Router>
     );
   }
 }
